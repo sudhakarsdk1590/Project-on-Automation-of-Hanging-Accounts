c36cf739cd5cbe79f2209919ac570d4e236087df
sudhakarsdk1590 <sudhakarvisam26@gmail.com>
2026-04-27 15:00:09 +0530

    Added Api response file

--- src/main/java/com/sudhakarproject/dto/ApiErrorResponse.java
+++ src/main/java/com/sudhakarproject/dto/ApiErrorResponse.java
@@ -1,4 +1,17 @@
 package com.sudhakarproject.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class ApiErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
 }
