36c87af8aef477543b31db5fcb744def6e99f48c
sudhakarsdk1590 <sudhakarvisam26@gmail.com>
2026-04-29 11:48:49 +0530

    added new filter for extraction data from DB

--- src/main/java/com/sudhakarproject/summarytracker/SummaryTracker.java
+++ src/main/java/com/sudhakarproject/summarytracker/SummaryTracker.java
@@ -1,11 +1,12 @@
 package com.sudhakarproject.summarytracker;
 
 import com.sudhakarproject.pojo.Transactions;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.atomic.AtomicLong;
-
+@Slf4j
 @Component
 @StepScope
 public class SummaryTracker {
@@ -13,10 +14,13 @@ public class SummaryTracker {
     private AtomicLong total = new AtomicLong();
     private AtomicLong hanging = new AtomicLong();
 
-    public void track(Transactions transactions) {
+    public void track(Transactions tx) {
         total.incrementAndGet();
-        if ("HANGING".equals(transactions.getStatus())) {
+        if (tx.getDpd() != null && tx.getDpd() > 30) {
             hanging.incrementAndGet();
+        }
+        if (total.get() % 1000 == 0) {
+            log.info("Processed={}, Hanging={}", total.get(), hanging.get());
         }
     }
 
